cf3ba4b8002240ef10a0a5fc9c6c3fa3711d721d
Emmanuel López <lopezrodemmanuel@gmail.com>
2022-03-26 15:41:51 -0500

    Resuelto

---
+++ televisores/tv.py
@@ -0,0 +1,72 @@
+class TV:
+    _numTV = 0
+
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._volumen = 1
+        self._precio = 500
+        TV._numTV += 1
+
+    # ----- GET -----
+    def getMarca(self):
+        return self._marca
+
+    def getControl(self):
+        return self._control
+
+    def getPrecio(self):
+        return self._precio
+
+    def getVolumen(self):
+        return self._volumen
+
+    def getCanal(self):
+        return self._canal
+
+    def getEstado(self):
+        return self._estado
+
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
+    
+    # ----- SET -----
+    def setMarca(self, marca):
+        self._marca = marca
+
+    def setControl(self, control):
+        self._control = control
+    
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def setVolumen(self, volumen):
+        self._volumen = volumen
+
+    def setCanal(self, canal):
+        self._canal = canal
+    
+    # ----- OTROS -----
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+
+    def canalUp(self):
+        if self._estado and self._canal < 120:
+            self._canal += 1
+
+    def canalDown(self):
+        if self._estado and self._canal > 1:
+            self._canal -= 1
+
+    def volumenUp(self):
+        if self._estado and self._volumen < 7:
+            self._volumen += 1
+
+    def volumenDown(self):
+        if self._estado and self._volumen > 0:
+            self._volumen -= 1
